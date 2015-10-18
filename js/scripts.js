e0445c265b5a7e408057181bd9f338eced53577d
jangosto <juan.angosto.h@gmail.com>
2015-10-18 14:59:28 +0200

    Removed some functions in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -52,10 +52,10 @@ $(document).ready(function() {
     $("#boton_menu").css("display","block");
   });
   // Cierra tarjetas
-  $(".descartar").each(function() {
+/*  $(".descartar").each(function() {
     $(this).click(function(){
       $(this).parents(".tarjeta").parent().slideToggle();
-    });
+    });*/
   });
   // Desplegables
   $(".desplegador").each(function() {
